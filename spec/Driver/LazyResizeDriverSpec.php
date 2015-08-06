ca118b98be15440d013dc8ffdf9373c3908ce1c3
fen <fen@desmart.com>
2015-08-06 16:10:53 +0200

    Add basic functionallity to LazyResizeDriver
    It creates a copy of the original image (wihtout any changes), saves it to a different location and returns the image.

--- spec/Driver/LazyResizeDriverSpec.php
+++ spec/Driver/LazyResizeDriverSpec.php
@@ -2,11 +2,24 @@
 
 use Prophecy\Argument;
 use PhpSpec\ObjectBehavior;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Intervention\Image\ImageManager as Image;
 
 class LazyResizeDriverSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function let(FilesystemAdapter $storage, Image $image)
     {
-        $this->shouldHaveType('DeSmart\ResizeImage\Driver\LazyResizeDriver');
+        $this->beConstructedWith($storage, $image);
+    }
+
+    function it_checks_if_the_file_exists(FilesystemAdapter $storage)
+    {
+        $storage->exists('foo.jpg')->shouldBeCalled()->willReturn(false);
+
+        $this->exists('foo.jpg')->shouldReturn(false);
+
+        $storage->exists('foo.jpg')->shouldBeCalled()->willReturn(true);
+
+        $this->exists('foo.jpg')->shouldReturn(true);
     }
 }
--- src/Driver/LazyResizeDriver.php
+++ src/Driver/LazyResizeDriver.php
@@ -1,5 +1,53 @@
 <?php namespace DeSmart\ResizeImage\Driver;
 
+use DeSmart\ResizeImage\UrlObject;
+use DeSmart\ResizeImage\ImageConfig;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Intervention\Image\ImageManager as Image;
+
 class LazyResizeDriver implements DriverInterface
 {
+
+    /**
+     * @var FilesystemAdapter
+     */
+    protected $storage;
+
+    /**
+     * @var Image
+     */
+    protected $image;
+
+    public function __construct(FilesystemAdapter $storage, Image $image)
+    {
+        $this->storage = $storage;
+        $this->image = $image;
+    }
+
+    /**
+     * Returns true if the file exists.
+     *
+     * @param string $fileName
+     * @return bool
+     */
+    public function exists($fileName)
+    {
+        return $this->storage->exists($fileName);
+    }
+
+    /**
+     * Creates an image based on ImageConfig, modifies and returns it.
+     *
+     * @param UrlObject $urlObject
+     * @param ImageConfig $imageConfig
+     * @return mixed
+     */
+    public function createImage(UrlObject $urlObject, ImageConfig $imageConfig)
+    {
+        $image = $this->image->make($this->storage->get($urlObject->getFullPath()))->resize(300, 200);
+
+        $this->storage->put('resize/'.$urlObject->getFullPath(), $image->response());
+
+        return $image;
+    }
 }
