3f3a29a919184cd3a58d4b079f56c59c434dc76c
fen <fen@desmart.com>
2015-08-06 16:09:24 +0200

    Add two methods to DriverInterface

--- src/Driver/DriverInterface.php
+++ src/Driver/DriverInterface.php
@@ -1,5 +1,30 @@
 <?php namespace DeSmart\ResizeImage\Driver;
 
+use DeSmart\ResizeImage\UrlObject;
+use DeSmart\ResizeImage\ImageConfig;
+
+/**
+ * Resize image driver interface.
+ *
+ * @package DeSmart\ResizeImage\Driver
+ */
 interface DriverInterface
 {
+
+    /**
+     * Returns true if the file exists.
+     *
+     * @param string $fileName
+     * @return bool
+     */
+    public function exists($fileName);
+
+    /**
+     * Creates an image based on ImageConfig, modifies and returns it.
+     *
+     * @param UrlObject $urlObject
+     * @param ImageConfig $imageConfig
+     * @return mixed
+     */
+    public function createImage(UrlObject $urlObject, ImageConfig $imageConfig);
 }
